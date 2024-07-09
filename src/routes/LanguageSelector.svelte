ee3a7be1dd150d87ce507f830ee3c21a07f1ad8b
luthfizone <luthfizonedev@gmail.com>
2024-07-09 21:32:42 +0700

    chore: remove log user input

--- src/routes/LanguageSelector.svelte
+++ src/routes/LanguageSelector.svelte
@@ -14,8 +14,6 @@
 
 	onMount(() => {
 		const userAgent = window.navigator.userAgent;
-		console.log(userAgent);
-		//
 		if (userAgent.includes('Chrome') && !userAgent.includes('Mobile')) {
 			showFlags.set(false);
 		}
