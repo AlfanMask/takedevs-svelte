8102b4ed2872fe1b407ea06ed97af92bb04bc691
luthfizone <luthfizonedev@gmail.com>
2024-07-09 21:27:24 +0700

    fix: deploy redirect to param en

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		goto('/en');
+	});
+</script>
+
+<div class="w-full h-screen bg-base"></div>
